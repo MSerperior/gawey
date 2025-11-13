7e1bb11e5b5ce367282c42b6e118c3f65a95622b
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2025-11-13 12:42:55 +0700

    feat: add search bar and carousel to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,48 @@
+import Banner from "@/components/custom/banner";
 import Card from "@/components/custom/card";
 import MiniCard from "@/components/custom/mini-card";
+import { CardContent } from "@/components/ui/card";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import Link from "next/link";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <div>
-      <div id="rekomendasi" className="flex flex-col items-center px-20">
+
+      <div id="banner" className="bg-blue-100 flex justify-center px-20 py-12">
+        <Carousel className="w-full max-w-5xl">
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <Banner />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+
+      <div id="search" className="flex flex-col md:flex-row items-center gap-4 md:gap-10 py-4 w-full text-gray-600 bg-white px-20 text-nowrap">
+        <div className="flex items-center gap-10">
+          <Link href={"/"}>Updates</Link>
+          <Link href={"/"}>Insight</Link>
+          <Link href={"/"}>HR</Link>
+          <Link href={"/"}>Tech</Link>
+          <Link href={"/"}>Business & Financial</Link>
+        </div>
+        <div className="relative flex items-center border border-gray-300 rounded-lg w-full">
+          <FaMagnifyingGlass size={20} className="absolute left-4" />
+          <input
+            type='text'
+            placeholder="cari artikel disini"
+            className="pl-10 w-full h-10 focus:outline-none"
+          />
+        </div>
+      </div>
+
+      <div id="rekomendasi" className="flex flex-col items-center bg-gray-100 px-20">
         <div className="text-3xl md:text-4xl text-blue-950 font-bold pt-12">Rekomendasi buat kamu</div>
         <div className="grid md:grid-cols-3 pt-9 gap-6">
           <Card />
@@ -25,7 +62,7 @@ export default function Home() {
           <MiniCard />
           <MiniCard />
         </div>
-        
+
         <Link className="rounded-lg px-8 py-4 my-4 bg-primary text-white self-center" href={"/"}>Lihat Semua</Link>
       </div>
     </div>
---
+++ src/components/custom/banner.jsx
@@ -0,0 +1,16 @@
+export default function Banner() {
+  return (
+    <div>
+      <div className="absolute top-12 w-full text-white z-20 px-12">
+        <div className="flex flex-col gap-2">
+          <div className="text-2xl md:text-4xl font-bold">Gawey telah hadir!</div>
+          <div className="text-2xl md:text-4xl font-bold">Aplikasi HRIS dalam 1 genggaman</div>
+          <div className="font-bold">14 Februari 2025</div>
+        </div>
+        <div className="border border-white py-2 px-6 w-fit rounded-lg text-white mt-16">Baca Sekarang!</div>
+      </div>
+      <div className="absolute bg-primary rounded-lg w-full h-82 z-10 mask-r-from-0% mask-r-to-80%"></div>
+      <img src="image-placeholder.png" className="h-82 w-full object-none rounded-lg" />
+    </div>
+  );
+}
