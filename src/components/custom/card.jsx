826f7eeccf5c5593ca50e5e064c938d2e8a2220e
Dany Christian <45038103+MSerperior@users.noreply.github.com>
2025-11-13 12:43:23 +0700

    style: add mask to image card

--- src/components/custom/card.jsx
+++ src/components/custom/card.jsx
@@ -1,7 +1,10 @@
 export default function Card() {
   return (
     <div>
-      <img src="image-placeholder.png" className="rounded-xl pb-3"></img>
+      <div className="relative">
+        <div className="absolute w-full rounded-lg bg-primary h-56 z-10 mask-r-from-0% mask-r-to-80%"></div>
+        <img src="image-placeholder.png" className="rounded-xl h-56 w-full mb-3 z-0 object-cover"></img>
+      </div>
       <div className="text-gray-600 text-lg pb-2">Human Resource</div>
       <div className="font-bold text-blue-950 text-xl pb-2">Lorem ipsum dolor sit amet, consecte adipiscing elit.</div>
       <div className="flex justify-between  text-blue-300 font-bold">
--- src/components/custom/mini-card.jsx
+++ src/components/custom/mini-card.jsx
@@ -1,7 +1,10 @@
 export default function MiniCard() {
   return (
     <div className="flex flex-row gap-6">
-      <img src="image-placeholder.png" className="rounded-xl pb-3 h-30"></img>
+      <div className="relative">
+        <div className="absolute w-full rounded-lg bg-primary h-30 z-10 mask-r-from-0% mask-r-to-100%"></div>
+        <img src="image-placeholder-2.jpg" className="rounded-xl w-48 mb-3 h-30 object-cover"></img>
+      </div>
       <div>
         <div className="text-gray-600 text-lg pb-1">Human Resource</div>
         <div className="font-bold text-blue-950 text-xl">Lorem ipsum dolor sit amet, consecte adipiscing elit.</div>
